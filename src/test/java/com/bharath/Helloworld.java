0dff2b66883eb5dfcd0140e17334ffbdacb8d46e
DharaniBharath <bharathddb421@gmail.com>
2025-11-29 11:42:44 +0530

    creating class

---
+++ src/test/java/com/bharath/Helloworld.java
@@ -0,0 +1,17 @@
+package com.bharath;
+
+import io.restassured.RestAssured;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        RestAssured
+                .given().baseUri("https://google.com").log().all()
+                .when().get()
+                .then().statusCode(200).log().all();
+    }
+}
+
+
+
+
